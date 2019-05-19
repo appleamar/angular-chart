4f720f9d3dccd2e90b141ce2b044ecbb4e26533b
appleamar <appleamar@gmail.com>
2019-05-19 13:33:56 +0530

    chartjs added

--- python-chart/src/app/app.module.ts
+++ python-chart/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
 
+const routes: Routes = [
+  {path: 'linechart', component: LineChartComponent},
+  {path: '*', component: LineChartComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
